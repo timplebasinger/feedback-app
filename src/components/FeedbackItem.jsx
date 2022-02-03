59ae26a2014a4ba9926761d527358b1b9349ad30
Timple Basinger <tbasinger@protonmail.com>
2022-02-03 15:40:12 -0600

    Create feedback data file, Feedback List Component

--- src/components/FeedbackItem.jsx
+++ src/components/FeedbackItem.jsx
@@ -1,17 +1,11 @@
-import React from 'react';
-import { useState } from 'react'
-
-function FeedbackItem() {
-  const [rating, setRating] = useState(7)
-  const [text, setText] = useState('This is an example of a feedback item.')
 
 
+function FeedbackItem({item}) {
+  
   return (
   <div className='card'>
-    <div className="num-display">{rating}</div>
-    <div className="text-display">
-       
-    </div>
+    <div className='num-display'>{item.rating}</div>
+    <div className="text-display">{item.text}</div>
   </div>)
 }
 
