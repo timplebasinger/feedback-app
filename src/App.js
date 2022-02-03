7dbd900be820d7241b1e607919ed4b06ac7c0afa
Timple Basinger <tbasinger@protonmail.com>
2022-02-03 14:27:29 -0600

    Setup Feedback Item Component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import Header from "./components/Header"
-
+import FeedbackItem from "./components/FeedbackItem"
 
 function App() {
   return (
     <>
     <Header />
     <div className='container'>
-    <h1>Hello from the App Component</h1>
+    <FeedbackItem />
     </div>
     </>
   )
