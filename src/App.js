11b6b86cab0f61abe064c2162337ce0341975ac0
Timple Basinger <tbasinger@protonmail.com>
2022-02-03 12:54:33 -0600

    Setup Header Component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,14 @@
+import Header from "./components/Header"
+
+
 function App() {
   return (
+    <>
+    <Header />
+    <div className='container'>
     <h1>Hello from the App Component</h1>
+    </div>
+    </>
   )
 }
 
